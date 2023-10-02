74cfa28628678233610836d32093e34de0be6abb
yakofu <132476248+yakofu@users.noreply.github.com>
2023-10-02 21:40:42 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+
+
+//client section owl carousel
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    navText: [
+        '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
